b1049e184509368dc47a994f01bdbdbc066b135c
Favot <70208301+Favot@users.noreply.github.com>
2022-08-01 23:28:38 +0200

    Fix URI production

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const uri =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? "https://lift-off-tutorial-server.herokuapp.com/"
-    : "localhost:4000";
+    : " http://localhost:4000/";
 
+console.log("🚀 ~ file: index.js ~ line 8 ~ uri", uri);
 const client = new ApolloClient({
   uri,
   cache: new InMemoryCache(),
